ae7bc7f933eac778eb0b6d351fde82d10bacaf63
irenekyc <gugu_irene@hotmail.com>
2020-06-25 13:55:52 +0200

    Updates

---
+++ precache-manifest.ba397d44186c4e4b7c5f97a65440e274.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7305f57c6834d1fdf9bd6ca2a78c75c",
+    "url": "/PopGerms-landing-page/index.html"
+  },
+  {
+    "revision": "b89f266504eb78da8c0b",
+    "url": "/PopGerms-landing-page/static/css/2.b0d70484.chunk.css"
+  },
+  {
+    "revision": "9a6c87925b0ac6df9ffd",
+    "url": "/PopGerms-landing-page/static/css/main.ed8a0a98.chunk.css"
+  },
+  {
+    "revision": "b89f266504eb78da8c0b",
+    "url": "/PopGerms-landing-page/static/js/2.0aac2a9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PopGerms-landing-page/static/js/2.0aac2a9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6c87925b0ac6df9ffd",
+    "url": "/PopGerms-landing-page/static/js/main.bec6bf73.chunk.js"
+  },
+  {
+    "revision": "c6cfae2bfd38f689c71c",
+    "url": "/PopGerms-landing-page/static/js/runtime-main.26ebf5c2.js"
+  }
+]);
